91d06e5ea2227f7c3540100818fa9cbedf13bc69
Brian Mann <brian@osysit.com>
2013-03-10 23:26:40 -0400

    wrote the first marionette install generator to setup initial folder structure and app file

---
+++ generators/marionette/install/install_generator.rb
@@ -0,0 +1,50 @@
+module Marionette
+	module Generators
+		class InstallGenerator < Rails::Generators::NamedBase
+			# include Backbone::Generators::ResourceHelpers
+
+			source_root File.expand_path("../templates", __FILE__)
+
+			# desc "This generator installs backbone.js with a default folder layout in app/assets/javascripts/backbone"
+
+			# class_option :skip_git, :type => :boolean, :aliases => "-G", :default => false,
+			# 												:desc => "Skip Git ignores and keeps"
+
+			# def inject_backbone
+			# 	inject_into_file "app/assets/javascripts/application.js", :before => "//= require_tree" do
+			# 		"//= require underscore\n//= require backbone\n//= require backbone_rails_sync\n//= require backbone_datalink\n//= require backbone/#{application_name.underscore}\n"
+			# 	end
+			# end
+			#
+		
+			def	append_lib_dependencies
+				%w{underscore backbone marionette}.each do |lib|
+					append_to_file "app/assets/javascripts/application.js" do
+						"\n//= require lib/#{lib}"
+					end
+				end
+			end
+			
+			def create_dir_layout
+				%w{apps config entities views}.each do |dir|
+					empty_directory "app/assets/javascripts/backbone/#{dir}"
+				end
+			end
+			
+			def append_app_files 
+				append_to_file "app/assets/javascripts/application.js" do
+					"//= require_tree ./backbone/config\n" +
+					"//= require backbone/app\n" +
+					"//= require_tree ./backbone/apps\n" +
+					"//= require_tree ./backbone/entities\n"
+				end
+			end
+			
+			def create_app_file
+				@current_user = yes? "Do you need to work with an authenticated current user (y/n)"
+				template "app.js.coffee", "app/assets/javascripts/backbone/app.js.coffee"
+			end
+
+		end
+	end
+end
---
+++ generators/marionette/install/templates/app.js.coffee
@@ -0,0 +1,27 @@
+@<%= file_name.camelize %> = do (Backbone, Marionette) ->
+	
+	App = new Marionette.Application	
+	<% if @current_user %>
+	# App.rootRoute = Routes.users_path()
+	# 
+	# App.on "initialize:before", (options) ->
+	# 	@currentUser = App.request "set:current:user", options.currentUser
+	
+	# App.reqres.addHandler "get:current:user", ->
+	# 	App.currentUser
+	<% end %>	
+	App.addRegions
+		headerRegion: "#header-region"
+		mainRegion: "#main-region"
+		footerRegion: "#footer-region"
+	
+	App.addInitializer ->
+		App.module("HeaderApp").start()
+		App.module("FooterApp").start()
+	
+	App.on "initialize:after", (options) ->
+		if Backbone.history
+			Backbone.history.start()
+			# @navigate(@rootRoute, trigger: true) if @getCurrentRoute() is ""
+	
+	App
