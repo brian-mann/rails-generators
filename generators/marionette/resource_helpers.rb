870ce6ec677cde201c87a35c0e87598fc7cee48c
Brian Mann <brian@osysit.com>
2013-03-13 00:58:21 -0400

    added resource helpers to all generator modules

---
+++ generators/marionette/resource_helpers.rb
@@ -0,0 +1,64 @@
+module Marionette
+  module Generators
+    module ResourceHelpers
+      
+      # def backbone_path
+      #   "app/assets/javascripts/backbone"
+      # end
+      # 
+      # def model_namespace
+      #   [js_app_name, "Models", class_name].join(".")
+      # end
+      # 
+      # def singular_model_name
+      #   uncapitalize singular_name.camelize
+      # end
+      # 
+      # def plural_model_name
+      #   uncapitalize(plural_name.camelize)
+      # end
+      # 
+      # def collection_namespace
+      #   [js_app_name, "Collections", plural_name.camelize].join(".")
+      # end
+      # 
+      # def view_namespace
+      #   [js_app_name, "Views", plural_name.camelize].join(".")
+      # end
+      # 
+      # def router_namespace
+      #   [js_app_name, "Routers", plural_name.camelize].join(".")
+      # end
+      # 
+      # def jst(action)
+      #   "backbone/templates/#{plural_name}/#{action}"
+      # end
+
+			def embed_file(source, indent='')
+				IO.read(File.join(self.class.source_root, source)).gsub(/^/, indent)
+			end
+
+			def embed_template(source, indent='')
+				template = File.join(self.class.source_root, source)
+				ERB.new(IO.read(template), nil, '-').result(binding).gsub(/^/, indent)
+			end
+      
+      def js_app_name
+        application_name.camelize
+      end
+      
+      def application_name
+        if defined?(Rails) && Rails.application
+          Rails.application.class.name.split('::').first
+        else
+          "app"
+        end
+      end
+      
+      # def uncapitalize(str)
+      #   str[0, 1].downcase << str[1..-1]
+      # end
+      
+    end
+  end
+end
